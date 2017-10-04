aec5d51752490cc8e1af7ac31113750f8bd80d38
Craig Buckler <craig@optimalworks.net>
2017-10-04 14:38:28 +0100

    content and image updates

--- tacs/template/pageimage.php
+++ tacs/template/pageimage.php
@@ -1,10 +1,10 @@
 [[<?php
 // page images and links
 $pi = array(
-	'bathroom' => '<li class="bathroom"><a href="' . $page['bathroom'] . '">beautiful bathrooms</a></li>',
-	'kitchen' => '<li class="kitchen"><a href="' . $page['kitchen'] . '">superior kitchens</a></li>',
-	'interior' => '<li class="interior"><a href="' . $page['extensions'] . '">impressive interiors</a></li>',
-	'extensions' => '<li class="exterior"><a href="' . $page['extensions'] . '">exceptional extensions</a></li>',
+	'bathroom' => '<li class="bathroom2"><a href="' . $page['bathroom'] . '">beautiful bathrooms</a></li>',
+	'kitchen' => '<li class="kitchen2"><a href="' . $page['kitchen'] . '">superior kitchens</a></li>',
+	'interior' => '<li class="extension"><a href="' . $page['extensions'] . '">impressive interiors</a></li>',
+	'extensions' => '<li class="extension2"><a href="' . $page['extensions'] . '">exceptional extensions</a></li>',
 	'commercial' => '<li class="commercial"><a href="' . $page['projects'] . '">project management</a></li>'
 );
 
